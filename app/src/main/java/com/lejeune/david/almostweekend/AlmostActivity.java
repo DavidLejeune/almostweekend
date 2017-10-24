0332a714867bd8722401a1fa1649ebc32b94e59b
DavidLejeune <Lejeune.David@outlook.com>
2017-10-24 16:59:39 +0200

    almost activity

---
+++ app/src/main/java/com/lejeune/david/almostweekend/AlmostActivity.java
@@ -0,0 +1,13 @@
+package com.lejeune.david.almostweekend;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AlmostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_almost);
+    }
+}
